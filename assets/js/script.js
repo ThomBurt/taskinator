361432807fb2cd481568c69a841f8018a4acc1e9
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2021-12-14 16:18:58 -0500

    event object

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,19 @@ console.log(buttonEl);
 
 
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(){
+    event.preventDefault();  //evnet object - this is to be sure to override the default, which in this case, stops the browser from refreshing each time you hit 'add task'
+
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+  tasksToDoEl.appendChild(listItemEl); // append child, this basically means enter the elements
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
