ec234acc8c63bbe67f3ff85b6335edb9070af90a
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2021-12-15 13:36:49 -0500

    add ability to submit task forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,14 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+
+// check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset(); //this resets the form after you enter a task... it is not breaking a bug or an issue, it is just for user experience
+
   // package up data as an object
   var taskDataObj = {
       name: taskNameInput,
